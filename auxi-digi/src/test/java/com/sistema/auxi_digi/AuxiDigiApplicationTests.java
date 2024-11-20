e3481ee1269a33363f3f8437f1a451355c99578b
guy djoumessi <gmdnegocio@gmail.com>
2024-11-20 14:02:54 -0300

    Initial commit

---
+++ auxi-digi/src/test/java/com/sistema/auxi_digi/AuxiDigiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.auxi_digi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuxiDigiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
